9586501983dd2165b4656e8e2f0fef8906a06426
alpha-oliveira <comuneiro@gmail.com>
2017-05-11 22:21:48 +0100

    monitorize form

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,25 +1,32 @@
 (function () {
     const zen = document.querySelector('.btn_lecture_zen');
+    const body =  document.querySelector('body');
+    let script = document.createElement('script');
+    
     const clickEvent = new MouseEvent("click", {
         "view": window,
         "bubbles": true,
         "cancelable": false
     });
     zen.dispatchEvent(clickEvent);
-    checkDOMChange();
-
+   
+    const injectedCode = `
     function checkDOMChange() {
-        
+            let form = document.querySelector('#offre_decouverte_form');
             let layers = document.querySelectorAll('.fond_overlay');
-            if (layers) {
+            if (layers && form) {
                layers.forEach(function(layer) {
                     layer.setAttribute('hidden', 'true');
                });
-               document.querySelector('#offre_decouverte_form').setAttribute('hidden', 'true');
+               form.setAttribute('hidden', 'true');
                 return;
             }
       
     setTimeout(checkDOMChange, 100);
     }
+ checkDOMChange();
+`;
+script.textContent= injectedCode;
+body.appendChild(script);
 
 })()
